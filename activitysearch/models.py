c38ec7317dabe8d6af0de39ef84a9419a5afc4c1
Jeremy Schreck <schreck.jeremy@gmail.com>
2017-12-28 23:04:32 -0800

    Add __str__ descriptions to models

--- activitysearch/models.py
+++ activitysearch/models.py
@@ -4,11 +4,17 @@ class Activity(models.Model):
     name = models.CharField(max_length=200)
     popularity = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 class Location(models.Model):
     city = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     latitude = models.IntegerField(default=0)
     longitude = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.city + "," + self.country
 
 class Trip(models.Model):
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE) # maybe change on_delete to another option
@@ -17,4 +23,6 @@ class Trip(models.Model):
     rating = models.IntegerField(default=0)
     distance = models.FloatField(default=0) # temporary placeholder for distance todo: derive distance from location + current location
 
+    def __str__(self):
+        return str(self.activity) + "in" + str(self.location)
 #todos: trip dates, seasonal trip info/ratings
